697e4cb6e92d521ebbc9cc7a1a0092b237272d7e
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2024-04-03 16:57:40 +0200

    Revert "ajout module env"
    This reverts commit 6ac303b6e5590b100c277e749d4c9f4c4a39c7cb.

--- expo-env.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="expo/types" />
-
-const API_KEY=""
-const API_URL="https://api.meaningcloud.com/topics-2.0"
-
-declare module '@env' {
-    export const API_KEY: string;
-    export const API_URL: string;
- }
