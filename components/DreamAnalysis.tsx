0bb0c93214449a275113779d72dbdf43abe607bf
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2024-04-03 16:37:03 +0200

    test

--- components/DreamAnalysis.tsx
+++ components/DreamAnalysis.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from "react";
 import { View, Text, Button, StyleSheet } from "react-native";
+import { API_KEY, API_URL } from '@env'
 
 export default function DreamAnalysis() {
   const [apiResponse, setApiResponse] = useState(null);
   const handleApiRequest = async () => {
     try {
-      const apiUrl = "https://api.meaningcloud.com/topics-2.0";
+      const apiUrl = API_URL;
       const language = "fr";
       const tmpDream = "Un truc important s'est passé avec gérard";
-      const apiKey = "bcd1ea9fc7ab51e2bc69736e28d91941";
+      const apiKey = API_KEY;
       const formdata = new FormData();
       formdata.append("key", apiKey);
       formdata.append("txt", tmpDream);
