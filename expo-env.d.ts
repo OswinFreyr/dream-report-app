f9faca1796d085ae71c6ffd74eb690aaad3069ee
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2024-04-03 16:52:03 +0200

    test

--- expo-env.d.ts
+++ expo-env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="expo/types" />
 
-const API_KEY=""
+const API_KEY="bcd1ea9fc7ab51e2bc69736e28d91941"
 const API_URL="https://api.meaningcloud.com/topics-2.0"
 
 declare module '@env' {
