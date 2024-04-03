6ac303b6e5590b100c277e749d4c9f4c4a39c7cb
OswinFreyr <142348764+OswinFreyr@users.noreply.github.com>
2024-04-03 16:46:31 +0200

    ajout module env

---
+++ expo-env.d.ts
@@ -0,0 +1,9 @@
+/// <reference types="expo/types" />
+
+const API_KEY=""
+const API_URL="https://api.meaningcloud.com/topics-2.0"
+
+declare module '@env' {
+    export const API_KEY: string;
+    export const API_URL: string;
+ }
